ec1dd61ebce760052bd221a1b0dba1a50bb66a85
Sahil Kumar <sahil23012000@gmail.com>
2025-01-30 16:20:53 +0530

    Enhance ProjectNavigation component with navigation buttons for live site and GitHub links

--- src/components/SingleProject/ProjectNavigation.jsx
+++ src/components/SingleProject/ProjectNavigation.jsx
@@ -1,5 +1,40 @@
-const ProjectNavigation = () => {
-  return <section className="section">ProjectNavigation</section>;
+import { RiArrowLeftLine } from "react-icons/ri";
+import NavigationButton from "../Utils/NavigationButton";
+import { SiGithub } from "react-icons/si";
+import { FaRegEye } from "react-icons/fa";
+
+// eslint-disable-next-line react/prop-types
+const ProjectNavigation = ({ liveSiteUrl, githubUrl }) => {
+  return (
+    <section className="section desktop:justify-between desktop:flex-row">
+      <div className="hidden desktop:block">
+        <NavigationButton
+          buttonText="Go back to projects"
+          href="/projects"
+          icon={RiArrowLeftLine}
+          iconPosition="before"
+          className="px-6"
+        />
+      </div>
+
+      <div className="flex flex-col gap-4 desktop:flex-row ">
+        <NavigationButton
+          buttonText="Preview Site"
+          href={liveSiteUrl}
+          icon={FaRegEye}
+          iconPosition="after"
+          className="px-6 desktop:mr-[2px]"
+        />
+        <NavigationButton
+          buttonText="Github URL"
+          href={githubUrl}
+          icon={SiGithub}
+          iconPosition="after"
+          className="px-6 bg-purple-600 text-black-600 hover:bg-purple-400 hover:text-black-600 desktop:scale-105"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default ProjectNavigation;
